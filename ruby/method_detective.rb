5cef560c3b6290f2572d022b2850ec1ccdddea5e
KarlThomas925 <karl.thomas925@gmail.com>
2016-12-18 21:36:42 -0600

    finish finding missing methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,28 +4,28 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
- "zom".<???>
+ "zom".insert(1, 'o')
 # => “zoom”
 
 "enhance".center(20)
 # => "    enhance    "
 
-"Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
-a = "suspects"
-a.prepend("the usual")
+
+"the usual".concat("suspects")
 #=> "the usual suspects"
 
-"suspects".<???>
+"suspects".prepend("the usual")
 # => "the usual suspects"
 
- "The case of the disappearing last letter".<???>
+ "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
- "The mystery of the missing first letter".<???>
+ "The mystery of the missing first letter".slice(1..-1)
 # => "he mystery of the missing first letter"
 
 "Elementary,    my   dear        Watson!".squeeze
@@ -35,5 +35,5 @@ a.prepend("the usual")
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-"How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
