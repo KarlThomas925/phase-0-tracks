46cf23f207ec8624eae2ed4e97bfe52adf3c8de0
KarlThomas925 <karl.thomas925@gmail.com>
2016-12-19 06:55:31 -0600

    add nested data structures and navigation for 5.4

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,37 @@
+desk = {
+	"printer" => [
+		'ink',
+		'paper',
+		'scanner',
+		'printer head',
+		],
+
+	"speakers" => [
+		'magnets',
+		'terminals',
+		'voicecoil',
+		'knobs',
+		'plastic body',
+		'speaker cones',
+		],
+
+	"drawers" => [
+		'Assignments',
+		'excess wires',
+		'playing cards',
+		'alcohol',
+		],
+	"computer" =>[
+		'cpu',
+		'gpu',
+	 	'SSD',
+	 	'Hard drive',
+	 ]
+}
+
+p desk['speakers']
+p desk['computer'][2]
+p desk['drawers'].push('headphones')
+
+
+p desk
