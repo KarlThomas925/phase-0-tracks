7efdd5af49e0d430b993a349497b51fcb9f74330
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-02 12:45:02 -0600

    Fix spacing

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -7,8 +7,8 @@ duchess = Puppy.new
 fido = Puppy.new
 spot = Puppy.new
 
-spot.class				 # returns Puppy in console.
+spot.class				# returns Puppy in console.
 duchess == fido			 # returns false in console. 
-fido.instance_of?(Array) # returns false in console.
-fido.instance_of?(Puppy) # returns true in console.
+fido.instance_of?(Array)	 # returns false in console.
+fido.instance_of?(Puppy) 	# returns true in console.
 spot.play_dead			 # returns undefined method error
