585761c2a8058aac516f407aa6801609278b4de5
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-01 20:02:39 -0600

    Refector code, change psuedo code, simplify methods.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,58 +1,52 @@
-# Method to create a list
-# input: string of items separated by spaces (example: "carrots apples cereal pizza")
-# steps: 
-  # print what is the name of list, get the user input, and create a hash. 
+# create the list outside of a method
+# create a method to add an item, 
+# 		Input(list, item, quantity)     
+# 		after testing, remove driver print code. 
+# create a method to remove an item  
+# 		input(list, item)  
+# 		after testing, remove driver print code. 
+# create a method to update and item 
+# 		input(list, item, quantity) 
+#  		after testing, remove driver print code. 
+# create a method to print the list.  
+# 		input(list)
+# 		after testing, remove driver print code. 
 
-  # print the list to the console [can you use one of your other methods here?]
-# output: [what data type goes here, array or hash?]
+grocery_list = Hash.new
+puts "Hello! welcome to grocery_list
+       ** to add an item **
+      *use  additem(grocery_list, item name in quotations, quantity of item)
+       "
+puts "       ** to remove an item **
+*use removeitem(grocery_list, item name in quotations) 
+ "
+puts "       ** to update an item **
+*use updateitem(grocery_list, item name in quotations, quantity) 
+  "
 
-# Method to add an item to a list
-# input: list, item name, and optional quantity
-# steps: define method, ask user for the item name as a string, ask user for the quantity as integer, 
-# => and put those into a hash. 
-# output:return the hash.
+puts "       ** to print out your completed list **
+*printlist(grocery_list) 
+ "
 
-# Method to remove an item from the list
-# input: list item name
-# steps: ask the user what they would like to remove, check to see if the key=>value pair exists,
-#return error message if it does not. 
-# output: item list hash
 
-# Method to update the quantity of an item
-# input: list, item name,  and optional quantity
-# steps:  ask user what items quantitiy they would like to change, check to see if that item is in existance,
-# access that items quantity value in order to change it
-# output: item list hash
-
-# Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
-
-def create_list
- list = Array.new
+def additem (list, item, quantity)
+	list[item] = quantity
+	p list
 end
 
-def add_item (list)
-	puts "Please name your item, and its quantity"
-	item = gets.chomp.split(' ')
-	items = {item[0].to_sym => item[1].to_i}
-	list.push(items)
-	return list
-end
 
-def remove_item (list)
-	puts "what item would you like to remove"
-	item = gets.chomp.to_sym
+def removeitem (list, item)
 	list.delete(item)
-	return list
+	p list
 end
 
-def update_item (list)
-	puts "which items quantity would you like to change?"
-	item = gets.chomp.to_sym
-	puts "what would you like to change the quantity to?"
-	quantity = gets.chomp.to_i
-	list[item] = quantity 
-	return list
+
+def updateitem (list, item, quantity)
+	list[item] = quantity
+	p list
+end
+
+
+def printoutlist (list)
+	list.each { |item, quantity| puts "There is  #{quantity} count of #{item}" } 
 end
