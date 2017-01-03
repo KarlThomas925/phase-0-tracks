25c859f2ca9a6711f4a6734f80673ded091b76b9
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-02 21:09:59 -0600

    Comment out old code, add mixin methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,6 +1,29 @@
-module Shout
-	# we'll put some methods here soon, but this code is fine for now!
-	def self.yell_angrily(words)
-    	words + "!!!" + " :("
-    end
+ module Shout
+# 	# we'll put some methods here soon, but this code is fine for now!
+# 	def self.yell_angrily(words)
+#     	words + "!!!" + " :("
+#     end
+
+#     def self.yell_happily(words)
+#     	words + "!!!" + ":D"
+#   	end
+	def roar(noise)
+		puts "IT MAKES ME WANNA PUT MY HANDS UP AND #{noise}"
+	end
+ end
+class bear
+	include roar
 end
+
+class lion
+	include roar
+end
+
+theo = bear.new
+theo.roar("RAAOOOAOEUOAIUIAUOEIAEIAOU")
+
+paul = lion.new
+paul.roar("meow")
+
+
+
