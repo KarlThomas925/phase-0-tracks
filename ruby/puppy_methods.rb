a67bcbf124fa956b73840edc85f5b27626c86d00
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-03 01:15:00 -0600

    Add puppy and kitten methods to the file.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,58 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
-
+def speak(i)
+	i.times {
+		puts "BORK"
+	}
 end
 
+def roll_over(name)
+	puts "#{name} flops around"
+end
+
+def dog_years(i)
+	puts i * 7
+end
+
+def shake(name)
+	puts "#{name} shakes your hand"
+end
+
+ def initialize
+    puts "Initializing new puppy instance..."  #mispelling init is no good, kills method dead. 
+ end
+end
+
+# doug = Puppy.new
+# doug.speak(20)
+# doug.roll_over("doug")
+# doug.shake("doug")
+
+class Kitten
+	def initialize
+		puts "init///BEGIN KITTEN PROGRAM"
+	end
+
+	def cute_levels(normalhumanlevel)
+		puts "Your normal level of cuteness is understoood as #{normalhumanlevel}"
+		puts "but to this kitten it is" normalhumanlevel * 47
+	end
+
+	def sleep
+		puts "it's always sleeping"
+	end
+end
+
+cats = Array.new
+	
+50.times {
+	cat5000 = Kitten.new
+	cats.push(cat5000)
+}
+
+cats.each do |cat4000|
+  puts "_________________________"
+	cat4000.cute_levels(rand(1..10))
+	cat4000.sleep
+	puts "_________________________"
+end
