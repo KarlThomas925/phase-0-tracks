6fc48a5d16e7b5db20f3547cdc160eb999fcbfee
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-08 23:44:07 -0600

    Complete car initialization, and complete 7.2

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,10 +1,36 @@
-var colors = ["blue", "REALLY BLUE","woah like that's blue", "teal"]
+var colors = ["blue", "REALLY BLUE","woah like that's blue", "teal"];
 
-var names = ["Ed","pot-OOOOOOOO", "c-bizkit", "dE"]
+var names = ["Ed","pot-OOOOOOOO", "c-bizkit", "dE"];
 
 colors.push("purple, i guess");
 
 names.push("horse");
+
+var megazord = {name: "Karl", color "WOAH MAN LIKE THATS BLUE"};
+//Why megazord? because i'm tired and we're combining things
+
+for (x = 0; x < colors.length; x++)
+{
+	megazord.name = names[x];
+	megazord.color = colors[x];
+
+	console.log(megazord);
+}
+
+function Cars(make,model,color,speed) {
+	console.log("Unveiling our new car:", thecar);
+
+	thecar.make = make;
+	thecar.model = model;
+	thecar.color = color;
+	thecar.speed = speed;
+
+	thecar.bark = function() { console.log("Woof!, 'Yeah sorry the car barks'"); };
+}
+
+var heyacar = new Cars("Volkswagon", "van", "red", 90)
+
+var anothercar = new Cars("Jeep", "Wrangler", "yellow", 120)
 
 
 
