02520532ff0068394d931f4f81a63b7e953254ed
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-08 22:26:05 -0600

    Finish 7.1, with haste

--- js/explore.js
+++ js/explore.js
@@ -3,3 +3,16 @@
 //un-split the array back into a string
 //return the result. 
 
+function reverses_word(s) {
+	var splitS = str.split("");
+
+	var reverseS = splitS.reverse();
+
+	var joinS = reverseS.joinS("");
+
+	return joinS;
+}
+
+// DRIVER CODE
+
+console.log (reverses_word("Karl"));
