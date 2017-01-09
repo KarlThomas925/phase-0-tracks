f0a4bb4afe30d253321acee9b0a0e3d8989604b9
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-08 23:17:55 -0600

    7.2, Complete release 0

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+var colors = ["blue", "REALLY BLUE","woah like that's blue", "teal"]
+
+var names = ["Ed","pot-OOOOOOOO", "c-bizkit", "dE"]
+
+colors.push("purple, i guess");
+
+names.push("horse");
+
+
+
