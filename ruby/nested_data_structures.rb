f849d8265d62703c28d29fed625f59007df70fac
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-16 05:33:26 -0600

    create new datastructure, print navigation results.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,42 +1,38 @@
-# Create desk hash, 
-# fill it with things on/in/apart of a desk
+# Create hash representative of skateboard types
+# fill it with the types, and brands of those types, and things about those brands. 
 # fill those things with things inside of those things, appropriately.
 # print navigation examples
 
-desk = {
-	"printer" => [
-		'ink',
-		'paper',
-		'scanner',
-		'printer head',
-		],
+Skateboard_types = { 
+	Skate: {
+		Pizza: {
+			Size: [7, 9],
+			Cutaway: 'no',
+			Board_only?: 'no'
+		},
+		Girl: {
+			Size: [7, 10],
+			Cutaway: 'yes',
+			Board_only?: 'yes'
+		}
+	},
+	Cruisers: {
+		Arbor: {
+			Size: [37, 44],
+			Cutaway: 'no',
+			Board_only?: 'no'
+		},
+		Earthwing: {Size: [33, 38],
+			Cutaway: 'yes',
+			Board_only?: 'no'
+		}
+	}
 
-	"speakers" => [
-		'magnets',
-		'terminals',
-		'voicecoil',
-		'knobs',
-		'plastic body',
-		'speaker cones',
-		],
 
-	"drawers" => [
-		'Assignments',
-		'excess wires',
-		'playing cards',
-		'alcohol',
-		],
-	"computer" =>[
-		'cpu',
-		'gpu',
-	 	'SSD',
-	 	'Hard drive',
-	 ]
+
 }
 
-p desk['speakers']
-p desk['computer'][2]
-p desk['drawers'].push('headphones')
-
+puts Skateboard_types[:Skate][:Cutaway]
+puts Skateboard_types[:Cruisers][:Arbor][:Size][0]
 
 p desk
