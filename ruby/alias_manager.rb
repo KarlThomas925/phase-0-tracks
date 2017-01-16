155e79ff9ea7230b2b03b17662a275c4fb3f56ba
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-16 08:22:52 -0600

    Finish loop, mix up method, and mapping of variables

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,4 +10,51 @@ loop_break = false
 names = []
 spy_names = []
 
+until loop_break
+
+  puts "Please give us your name, so we can give you an alias. (press ENTER to exit)"
+  input = gets.chomp
+
+  if input.empty?
+    loop_break = true
+  else
+    names << input
+    def Alias(name)
+      #pick it up, flip it, reverse it.
+      name = name.downcase #downcase is to prevent any issues with other methods
+      name = name.split(' ').reverse
+      name = name.join(' ')
+      name = name.split('')
+
+      #special arrays for special cases. 
+      vowels = ["a", "e", "i", "o", "u"]
+      edgecases = ["z", "d", "h", "n", "t"]
+      edgesolution = ["b", "f", "j", "p", "v"]
+
+      #the meat of the method, 
+      #mixing up and mapping the mixed up name to the results variable
+      results = name.map do |char|
+        if vowels.include?(char)
+          vowels.rotate(1)[vowels.index(char)]
+        elsif edgecases.include?(char)
+          edgesolution[edgecases.index(char)]
+        elsif char == " "
+          char == " "
+        else
+          char.next
+        end
+      end
+      results = results.join
+      results.split.map(&:capitalize).join(' ')
+    end
+    puts "Your Super secret, totally untrackable spy name is #{Alias(input)}"
+    spy_names << Alias(name)
+  end
+  names = names.map!{|x| x.to_sym}
+
+  names_HASH = names.zip(spy_names).to_h
+end
+
+names_HASH.each do |input,results|
+  puts"#{input} is also known as #{results}
 
