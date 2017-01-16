baaf52cc62431b2b4db580342a25ce74c3c8544f
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-15 18:54:49 -0600

    Create file and add psuedocode

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,3 +3,18 @@
 	# print out clients list of info
 	# ask client if they had put in the correct information for each variable. 
 	# print list. 
+client = Hash.new 
+ 
+ puts "What is your name? "
+ client[:clientname] = gets.chomp!
+ 
+ puts "What is yuour age? "
+ client[:clientage] = gets.to_i 
+ 
+ puts "How many children do you have?: "
+ client[:clientchildnum] = gets.to_i
+ 
+ puts "What style of decor do youprefer the most? "
+ client[:clientdecor] = gets.chomp!
+ 
+  p client
