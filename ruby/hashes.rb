63a4e92586482b6f51fb8f313a4919d52cfdb3db
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-15 21:15:18 -0600

    Finish the double check questionnaire

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -18,3 +18,42 @@ client = Hash.new
  client[:clientdecor] = gets.chomp!
  
   p client
+
+  puts "Do we have your name down right? (Type 'yes' to keep your name)"
+ tmpname = gets.chomp!
+
+ 	if(tmpname == "yes")
+   		puts "Keeping current name."
+ 	else
+   		client[:clientname] = tmpname
+ 	end
+ 
+ puts "Is your age correct?  (Type '00' to keep your age)"
+ tmpage = gets.to_i
+ 
+ 	if(tmpage == 00)
+   		puts "Keeping current age"
+ 	else
+   		client[:clientage] = tmpage
+ 	end
+ 
+ puts "Is the number of kids we have down correct? (Type '00' to keep your number of children)"
+ tmpkids = gets.to_i
+ 
+ 	if(tmpkids == 00)
+   		puts "Keeping current number of kids."
+ 	else
+   		client[:clientchildnum] = tmpkids
+ 	end
+ 
+ puts "Is your preferred decor style correct? (Type 'yes' to keep your decor theme)"
+ tmpdecor = gets.chomp!
+ 
+ 	if(tmpdecor == "yes")
+   		puts "Keeping current decor style."
+ 	else
+   		client[:clientdecor] = tmpdecor
+ 	end
+   
+ 
+ p client 
