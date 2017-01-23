efffb25cfad955962ad30f4cf405396939d37350
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-23 00:02:57 -0600

    Completed GPS with Arra

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,63 +1,100 @@
-# create the list outside of a method
-# create a method to add an item, 
-# 		Input(list, item, quantity)     
-# 		after testing, remove driver print code. 
-# create a method to remove an item  
-# 		input(list, item)  
-# 		after testing, remove driver print code. 
-# create a method to update and item 
-# 		input(list, item, quantity) 
-#  		after testing, remove driver print code. 
-# create a method to print the list.  
-# 		input(list)
-# 		after testing, remove driver print code. 
+# Method to create a list
+# 
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # [fill in any steps here] define new method - create list
+  # set default quantity- input , a single string, 
+  # turn this string into a hash , string.split will turn string into an array
+  # iterate through array setting each item as key and value to default of 1
+  #hash = Hash[item_array.map] {|x| [x, 1]}
+  # hash = Hash[array.collect { |item| [item, 1] } ]
+  # set default to 1
 
-grocery_list = Hash.new
-puts "Hello! welcome to grocery_list
-       ** to add an item **
-      *use  additem(grocery_list, item name in quotations, quantity of item)
-       "
-puts "       ** to remove an item **
-*use removeitem(grocery_list, item name in quotations) 
- "
-puts "       ** to update an item **
-*use updateitem(grocery_list, item name in quotations, quantity) 
-  "
+  # print hash
+  # print the list to the console [can you use one of your other methods here?]
+# output: hash { "apples" =>1 , so }
+def create_list(string)
+	item_array=string.split
+	#grocery_list=Hash[item_array.map {|x| [x,1]}]
+	hash = {}
+	item_array.each do |x|
+		
 
-puts "       ** to print out your completed list **
-*printlist(grocery_list) 
- "
-
-
-def additem (list, item, quantity)
-	list[item] = quantity
-	p list
+		hash[x]=1
+    #p grocery_list
+	end
+	return hash
 end
+ 
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps: define new method for list
+  # hash[item]= quantity
+# output: list ( hash)
 
+ def add_item(list,item, quantity=2)
+	list[item]= quantity
+	list
+end 
 
-def removeitem (list, item)
+# Method to remove an item from the list
+# input:list, item
+# steps:list.delete(item)
+# return list
+# output:list 
+def remove_item(list,item)
 	list.delete(item)
 	p list
 end
+#remove_item(grocery_list,"apples")
 
+# Method to update the quantity of an item
+#same as add_item
 
-def updateitem (list, item, quantity)
-	list[item] = quantity
+def update_item(list,item, quantity=2)
+	list[item]= quantity
 	p list
-end
+end 
+#update_item(grocery_list,"juice", 4)
 
+# Method to print a list and make it look pretty
+# input:list
+# steps:p list.each do |key,value|
+# there is value of key
+# output:printed list( explicit return)
 
-def printoutlist (list)
-	list.each { |item, quantity| puts "There is  #{quantity} count of #{item}" } 
-end
-# driver code for testing methods.
-# input(grocery_list,"Lemonade",2)
-# input(grocery_list,"Tomatoes",3)
-# input(grocery_list,"Onions",1)
-# input(grocery_list,"Ice Cream",4)
+def print(list)
+	list.each do |key,value|
+		puts "There is #{value} count of #{key}"
 
-# remove(grocery_list,"Lemonade")
+     end
+end 
+#print(grocery_list)
 
-# update(grocery_list,"Ice Cream",1)
+# define new method - create_list
+#create_list("apples juice oranges")
+#grocery_list = create_list("apples juice oranges")
+#puts add_item(create_list("apples juice oranges"),"chips", 2)
+#p add_item(grocery_list,"chips",2)
+#p add_item(grocery_list,"chocolate")
+#p grocery_list
+=begin 
+Create a new list
+Add the following items to your list
+Lemonade, qty: 2
+Tomatoes, qty: 3
+Onions, qty: 1
+Ice Cream, qty: 4
+Remove the Lemonade from your list
+Update the Ice Cream quantity to 1
+Print out your list (Is this readable and nice looking)?
+=end
+grocery_list = create_list(" ")
+add_item(grocery_list,"lemonade", 2)
+add_item(grocery_list,"Tomatoes", 3)
+add_item(grocery_list,"Onions", 1)
+add_item(grocery_list,"ice_cream", 4)
+remove_item(grocery_list, "lemonade")
+update_item(grocery_list, "ice_cream", 1)
+print(grocery_list)
 
-# printout(grocery_list)
