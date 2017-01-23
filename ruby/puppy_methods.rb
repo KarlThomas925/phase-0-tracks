b9aa2104f432f15c38639334b8109c81797b8eb7
KarlThomas925 <karl.thomas925@gmail.com>
2017-01-23 03:54:40 -0600

    refactored code and added new functions to Kitten class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,26 +1,25 @@
 class Puppy
 
-def speak(i)
-	i.times {
-		puts "BORK"
-	}
-end
+	def initialize
+	    puts "Initializing new puppy instance..."  #mispelling init is no good, kills method dead. 
+	end
 
-def roll_over(name)
-	puts "#{name} flops around"
-end
+	def speak(repetitions)
+		i.times {puts "BORK"}
+	end
 
-def dog_years(i)
-	puts i * 7
-end
+	def roll_over(name)
+		puts "#{name} flops around"
+	end
 
-def shake(name)
-	puts "#{name} shakes your hand"
-end
+	def dog_years(real_years)
+		puts real_years * 7
+	end
 
- def initialize
-    puts "Initializing new puppy instance..."  #mispelling init is no good, kills method dead. 
- end
+	def shake(name)
+		puts "#{name} shakes your hand"
+	end
+
 end
 
 # doug = Puppy.new
@@ -29,17 +28,33 @@ end
 # doug.shake("doug")
 
 class Kitten
+	
 	def initialize
 		puts "init///BEGIN KITTEN PROGRAM"
 	end
 
 	def cute_levels(normalhumanlevel)
+	  	@cat_level = normalhumanlevel * 47
+
 		puts "Your normal level of cuteness is understoood as #{normalhumanlevel}"
-		puts "but to this kitten it is" normalhumanlevel * 47
+		puts "but to this kitten it is #{@cat_level}" 
 	end
 
+	def eat(food)
+		if food.downcase == "dry"
+			puts "swats dry food out of bowl"
+		elsif food.downcase == "wet"
+			puts "cat chows down"
+		else
+			puts "does not know how eat"
+		end
+	end
 	def sleep
-		puts "it's always sleeping"
+	  if(@cat_level < 5*47)
+		  puts "it never sleeps"
+		else
+		  puts "it's always sleeping"
+		end
 	end
 end
 
@@ -54,5 +69,6 @@ cats.each do |cat4000|
   puts "_________________________"
 	cat4000.cute_levels(rand(1..10))
 	cat4000.sleep
+	cat4000.eat('wet')
 	puts "_________________________"
 end
